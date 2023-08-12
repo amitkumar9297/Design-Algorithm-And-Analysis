e6ae99a8da90515b3d85a3abe479be7467f13f06
amitkumar9297 <amitkumar790194@gmail.com>
2023-08-12 22:32:15 +0530

    divide and conquer based algorithms code

---
+++ 01_divide_and_conquer/01_binary_search.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void binarySearch(int arr[], int target)
+{
+
+    int start = 0;
+    int end = 8;
+    int mid = (start + end) / 2;
+    if (arr[mid] == target)
+    {
+        cout << arr[mid] << endl;
+    }
+    if (target > arr[mid])
+    {
+        start = mid + 1;
+    }
+    else
+    {
+        end = mid - 1;
+    }
+    mid = (start + end) / 2;
+    while (arr[mid] != target && end == 0)
+    {
+        if (target > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+    if (target == arr[mid])
+    {
+        cout << "your target is found at location : " << mid << endl;
+    }
+    else
+    {
+        cout << "your target is not found" << endl;
+    }
+}
+
+int32_t main()
+{
+    int arr[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    binarySearch(arr, 7);
+
+    return 0;
+}
---
+++ 01_divide_and_conquer/02_quick_sort.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int arr[], int i, int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+int partition(int arr[], int l, int r)
+{
+    int pivot = arr[r];
+    int i = l - 1;
+    cout << arr[i] << endl;
+
+    for (int j = l; j < r; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr, i, j);
+        }
+    }
+
+    swap(arr, i + 1, r);
+    return i + 1;
+}
+
+void quickSort(int arr[], int l, int r)
+{
+
+    if (l < r)
+    {
+        int pi = partition(arr, l, r);
+        quickSort(arr, l, pi - 1);
+        quickSort(arr, pi + 1, r);
+    }
+}
+
+int32_t main()
+{
+    int arr[5] = {5, 7, 1, 3, 2};
+    quickSort(arr, 0, 4);
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
